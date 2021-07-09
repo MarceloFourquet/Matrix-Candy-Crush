d6d2a3c79b9aabb7fda3c4b5a83cfafa47e1f13e
MarceloFourquet <marcelofourquet@gmail.com>
2021-07-09 08:25:50 -0300

    Final Commit

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 var filas = 9
 var columnas = 9
 var mat = new Array(filas);
-var element = 0;
+var element = 1;
 for (var f = 0; f < filas; f++) {
 	mat[f] = new Array(columnas);
 	for (var c = 0; c < columnas; c++) {
@@ -9,43 +9,44 @@ for (var f = 0; f < filas; f++) {
 	}
 }
 
-var matrix = document.getElementById("matrix");
+var app = document.getElementById("app");
 
-matrix.appendChild(getTitle("Recorrida natural"));
+var matrix1 = getElement("div", "", "left")
+matrix1.appendChild(getElement("h2", "Recorrida natural", "title"));
 for (var f = 0; f < filas; f++) {
-	var divElement = getRow();
+	var row = getElement("div", "", "row");
 	for (var c = 0; c < columnas; c++) {
-		divElement.appendChild(getCell(mat[f][c]))
+		row.appendChild(getElement("div", mat[f][c], "cell"));
 	}
-	matrix.appendChild(divElement);
+	matrix1.appendChild(row);
 }
+app.appendChild(matrix1);
 
-matrix.appendChild(getTitle("Recorrida solicitada"));
+var matrix2 = getElement("div", "", "right")
+matrix2.appendChild(getElement("h2","Recorrida solicitada", "title"));
 for (var c = columnas-1; c >= 0; c--) {
-	var divElement = getRow();
+	var row = getElement("div", "", "row");
 	for (var f = filas-1; f >= 0; f--) {
-		divElement.appendChild(getCell(mat[f][c]))
+		row.appendChild(getElement("div", mat[f][c], "cell"));
 	}
-	matrix.appendChild(divElement);
+	matrix2.appendChild(row);
 }
+app.appendChild(matrix2);
 
-function getTitle(title){
-	var h = document.createElement("h2");
-	var text = document.createTextNode(title);
-	h.appendChild(text);
-	return h;
-}
-
-function getCell(value){
-	var d = document.createElement("div");
-	var text = document.createTextNode(value);
-	d.classList.add("cell");
-	d.appendChild(text);
-	return d;
-}
-
-function getRow(){
-	var d = document.createElement("div");
-	d.classList.add("line");
-	return d;
+function getElement(type, text, classes){
+	var element = document.createElement(type);
+	if (String.toString(text) != ""){
+		var textNode = document.createTextNode(text);
+		element.appendChild(textNode);
+	}
+	if (Array.isArray(classes)){
+		classes.forEach(c => {
+			element.classList.add(c);
+		});
+	}else{
+		if (classes != "" && classes != undefined){
+			element.classList.add(classes);
+		}
+	}
+	return element;
 }
